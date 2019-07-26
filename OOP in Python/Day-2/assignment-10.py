d70837aa6baf9b8bed746fc2041aaf962078daf6
unknown <edu.omkar@gmail.com>
2019-07-26 13:08:05 +0530

    More efficient Program

--- OOP in Python/Day-2/assignment-10.py
+++ OOP in Python/Day-2/assignment-10.py
@@ -1,5 +1,5 @@
 #OOPR-Assgn-10
-
+#Efficient Program by Minal Kaminwar
 class CallDetail:
     def __init__(self, phoneno, called_no, duration, call_type):
         self.__phoneno=phoneno
@@ -13,13 +13,13 @@ class Util:
 
     def parse_customer(self,list_of_call_string):
         self.list_of_call_objects=[]
-        
         for each_call in list_of_call_string:
             call_detail_list=each_call.split(",")
-            obj=[]
-            for attribute in call_detail_list:
-                obj.append(attribute)
-            a=CallDetail(obj[0],obj[1],obj[2],obj[3])
+            # print(call_detail_list)
+            # obj=[]
+            # for attribute in call_detail_list:
+            #     obj.append(attribute)
+            a=CallDetail(call_detail_list[0],call_detail_list[1],call_detail_list[2],call_detail_list[3])
             self.list_of_call_objects.append(a)
 
 call='9990000001,9330000001,23,STD'
